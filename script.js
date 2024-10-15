5f61701eb43751a57b449bb5399a2358c4d2be87
dev-em-144 <113379855+dev-em-144@users.noreply.github.com>
2024-10-14 21:04:38 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 
-// var form = document.querySelector("#userForm");
-// var clientList = document.getElementById("client-list").value;
 
-// document.addEventListener('submit', function(e){
-//     e.preventDefault();
-
-//     var clientList = document.getElementById('client').value;
-
-// });
 
 
 var buttonSubmit = document.getElementById('button');
@@ -16,7 +8,17 @@ var buttonSubmit = document.getElementById('button');
 function employee() {
     
     var clientList = document.getElementById('client-list').value;
+    var datePicker = document.getElementById('date-picker').value;
+
+    var month = datePicker.slice(5,7);
+    var date  = datePicker.slice(8,10);
+    var year  = datePicker.slice(0,4);
+    var dateFormat = month+"/"+date+"/"+year;
+
+  
+    // Result Timesheet    
     document.getElementById('client').innerHTML = clientList;
+    document.getElementById('date').innerHTML = dateFormat;
 
 }
 
